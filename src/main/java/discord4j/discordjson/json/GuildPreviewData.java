613bce3c3838d6decb8257e0e6674d320dbb1009
David <bi-crosseur@hotmail.fr>
2020-03-22 18:29:52 +0100

    Supports Guild Preview data

---
+++ src/main/java/discord4j/discordjson/json/GuildPreviewData.java
@@ -0,0 +1,30 @@
+package discord4j.discordjson.json;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import org.immutables.value.Value;
+
+import java.util.List;
+import java.util.Optional;
+
+@Value.Immutable
+@JsonSerialize(as = ImmutableGuildPreviewData.class)
+@JsonDeserialize(as = ImmutableGuildPreviewData.class)
+public interface GuildPreviewData {
+
+    String id();
+    String name();
+    Optional<String> icon();
+    Optional<String> splash();
+    @JsonProperty("discovery_splash")
+    Optional<String> discoverySplash();
+    List<EmojiData> emojis();
+    List<String> features();
+    @JsonProperty("approximate_member_count")
+    int approximateMemberCount();
+    @JsonProperty("approximate_presence_count")
+    int approximatePresenceCount();
+    Optional<String> description();
+
+}
