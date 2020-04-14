b56d0f54130e980dc9c0011d99c141a5760f0248
David <bi-crosseur@hotmail.fr>
2020-04-14 04:04:50 +0200

    Update Invite Create Event Fields (#11)

--- src/main/java/discord4j/discordjson/json/gateway/InviteCreate.java
+++ src/main/java/discord4j/discordjson/json/gateway/InviteCreate.java
@@ -3,6 +3,7 @@ package discord4j.discordjson.json.gateway;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import discord4j.discordjson.json.PartialUserData;
 import discord4j.discordjson.json.UserData;
 import discord4j.discordjson.possible.Possible;
 import org.immutables.value.Value;
@@ -34,4 +35,10 @@ public interface InviteCreate extends Dispatch {
     boolean temporary();
 
     default Possible<UserData> inviter() { return Possible.absent(); }
+
+    @JsonProperty("target_user")
+    default Possible<PartialUserData> targetUser() { return Possible.absent(); }
+
+    @JsonProperty("target_user_type")
+    default Possible<Integer> targetUserType() { return Possible.absent(); }
 }
