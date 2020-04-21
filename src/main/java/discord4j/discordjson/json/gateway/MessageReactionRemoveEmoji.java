5772a599d0c8c7f0167cb2a3e331b0c5f02abd9d
Alex <2230292+ByteAlex@users.noreply.github.com>
2020-04-21 15:20:04 +0200

    Add dispatch for MessageReactionRemoveEmoji (#12)

---
+++ src/main/java/discord4j/discordjson/json/gateway/MessageReactionRemoveEmoji.java
@@ -0,0 +1,29 @@
+package discord4j.discordjson.json.gateway;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import discord4j.discordjson.json.EmojiData;
+import discord4j.discordjson.possible.Possible;
+import org.immutables.value.Value;
+
+@Value.Immutable
+@JsonSerialize(as = ImmutableMessageReactionRemoveEmoji.class)
+@JsonDeserialize(as = ImmutableMessageReactionRemoveEmoji.class)
+public interface MessageReactionRemoveEmoji extends Dispatch {
+
+    static ImmutableMessageReactionRemoveEmoji.Builder builder() {
+        return ImmutableMessageReactionRemoveEmoji.builder();
+    }
+
+    @JsonProperty("channel_id")
+    String channelId();
+
+    @JsonProperty("message_id")
+    String messageId();
+
+    @JsonProperty("guild_id")
+    Possible<String> guildId();
+
+    EmojiData emoji();
+}
