f2ae5cf606ad26fe7d68f51af011e3997b6d10a1
quanticc <abarcab.ivan@gmail.com>
2020-05-09 15:49:14 -0400

    Add max_concurrency field to SessionStartLimitData

--- src/main/java/discord4j/discordjson/json/SessionStartLimitData.java
+++ src/main/java/discord4j/discordjson/json/SessionStartLimitData.java
@@ -3,6 +3,7 @@ package discord4j.discordjson.json;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import discord4j.discordjson.possible.Possible;
 import org.immutables.value.Value;
 
 @Value.Immutable
@@ -18,4 +19,7 @@ public interface SessionStartLimitData {
     int remaining();
     @JsonProperty("reset_after")
     int resetAfter();
+    @JsonProperty("max_concurrency")
+    Possible<Integer> maxConcurrency();
+
 }
