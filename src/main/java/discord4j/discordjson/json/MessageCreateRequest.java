8d0e9d7519d071483b007a61076f8f04b43fb53c
Rosetta Roberts <rosettafroberts@gmail.com>
2020-07-26 17:32:41 -0600

    Refactored common code of MessageCreateRequest and WebhookExecuteRequest into separate interface.

--- src/main/java/discord4j/discordjson/json/MessageCreateRequest.java
+++ src/main/java/discord4j/discordjson/json/MessageCreateRequest.java
@@ -1,6 +1,5 @@
 package discord4j.discordjson.json;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import discord4j.discordjson.possible.Possible;
@@ -9,16 +8,11 @@ import org.immutables.value.Value;
 @Value.Immutable
 @JsonSerialize(as = ImmutableMessageCreateRequest.class)
 @JsonDeserialize(as = ImmutableMessageCreateRequest.class)
-public interface MessageCreateRequest {
-
+public interface MessageCreateRequest extends MessageSendRequestBase {
     static ImmutableMessageCreateRequest.Builder builder() {
         return ImmutableMessageCreateRequest.builder();
     }
 
-    Possible<String> content();
     Possible<Object> nonce();
-    Possible<Boolean> tts();
     Possible<EmbedData> embed();
-    @JsonProperty("allowed_mentions")
-    Possible<AllowedMentionsData> allowedMentions();
 }
