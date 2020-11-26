8830f7b67dc620c1a456d706bf0434759a875c51
David <bi-crosseur@hotmail.fr>
2020-11-26 23:14:38 +0100

    Support role tags (#37)
    * Support role tags
    * Update tests
    * What have we done

--- src/main/java/discord4j/discordjson/json/RoleData.java
+++ src/main/java/discord4j/discordjson/json/RoleData.java
@@ -2,6 +2,7 @@ package discord4j.discordjson.json;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import discord4j.discordjson.possible.Possible;
 import org.immutables.value.Value;
 
 @Value.Immutable
@@ -28,4 +29,6 @@ public interface RoleData {
     boolean managed();
 
     boolean mentionable();
+
+    Possible<RoleTagsData> tags();
 }
