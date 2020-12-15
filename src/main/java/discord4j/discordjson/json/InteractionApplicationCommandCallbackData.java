c95f07f9926822fdbd9b82f9dddb3cac0b2a5c8f
Izzy <undermybrella@abimon.org>
2020-12-16 10:49:33 +1100

    Implement InteractionResponseData and InteractionApplicationCommandCallbackData

---
+++ src/main/java/discord4j/discordjson/json/InteractionApplicationCommandCallbackData.java
@@ -0,0 +1,40 @@
+package discord4j.discordjson.json;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import discord4j.discordjson.possible.Possible;
+import org.immutables.value.Value;
+
+import java.util.List;
+
+@Value.Immutable
+@JsonSerialize(as = ImmutableInteractionApplicationCommandCallbackData.class)
+@JsonDeserialize(as = ImmutableInteractionApplicationCommandCallbackData.class)
+public interface InteractionApplicationCommandCallbackData {
+
+    static ImmutableInteractionApplicationCommandCallbackData.Builder builder() {
+        return ImmutableInteractionApplicationCommandCallbackData.builder();
+    }
+
+    /**
+     * is the response TTS
+     */
+    Possible<Boolean> tts();
+
+    /**
+     * message content
+     */
+    String content();
+
+    /**
+     * supports up to 10 embeds
+     */
+    Possible<List<EmbedData>> embeds();
+
+    /**
+     * allowed mentions object
+     */
+    @JsonProperty("allowed_mentions")
+    Possible<AllowedMentionsData> allowedMentions();
+}
