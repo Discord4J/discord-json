84ca42c4557e3f962efdcbd81c219cef9c775f34
Izzy <undermybrella@abimon.org>
2020-12-18 13:44:36 +1100

    Optimise imports

--- src/main/java/discord4j/discordjson/json/gateway/InteractionCreate.java
+++ src/main/java/discord4j/discordjson/json/gateway/InteractionCreate.java
@@ -3,9 +3,7 @@ package discord4j.discordjson.json.gateway;
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import discord4j.discordjson.json.ImmutableInteractionData;
 import discord4j.discordjson.json.InteractionData;
-import discord4j.discordjson.json.InteractionResponseData;
 import org.immutables.value.Value;
 
 @Value.Immutable
