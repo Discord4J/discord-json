7b355577cb1d9315aff3b3a338a1c68c12c2c9e4
Redstoneguy129 <public@cameronwhyte.me>
2020-12-27 23:40:02 +0000

    TemplateData
    Added Template Data for interaction with the Discord API.

---
+++ src/main/java/discord4j/discordjson/json/TemplateData.java
@@ -0,0 +1,38 @@
+package discord4j.discordjson.json;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import discord4j.discordjson.possible.Possible;
+import org.immutables.value.Value;
+
+import java.util.Optional;
+
+@Value.Immutable
+@JsonSerialize(as = ImmutableTemplateData.class)
+@JsonDeserialize(as = ImmutableTemplateData.class)
+public interface TemplateData {
+
+    static ImmutableTemplateData.Builder builder() {
+        return ImmutableTemplateData.builder();
+    }
+
+    String code();
+    String name();
+    Optional<String> description();
+    @JsonProperty("usage_count")
+    Integer usageCount();
+    @JsonProperty("creator_id")
+    String creatorId();
+    UserData creator();
+    @JsonProperty("created_at")
+    String createdAt();
+    @JsonProperty("updated_at")
+    String updatedAt();
+    @JsonProperty("source_guild_id")
+    String sourceGuildId();
+    @JsonProperty("serialized_source_guild")
+    PartialGuildData serializedSourceGuild();
+    @JsonProperty("is_dirty")
+    Optional<Boolean> isDirty();
+}
