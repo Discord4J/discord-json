5ee5739a2b14de58c8fa44aeae04eb84479674db
Redstoneguy129 <public@cameronwhyte.me>
2020-12-28 10:26:38 +0000

    PartialGuildData to GuildData
    Please work i beg

--- src/main/java/discord4j/discordjson/json/TemplateData.java
+++ src/main/java/discord4j/discordjson/json/TemplateData.java
@@ -31,7 +31,7 @@ public interface TemplateData {
     @JsonProperty("source_guild_id")
     String sourceGuildId();
     @JsonProperty("serialized_source_guild")
-    PartialGuildData serializedSourceGuild();
+    GuildData serializedSourceGuild();
     @JsonProperty("is_dirty")
     Optional<Boolean> isDirty();
 }
