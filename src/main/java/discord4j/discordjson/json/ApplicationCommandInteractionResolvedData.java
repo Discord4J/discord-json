5df41d7225015ee657428cdd6e7abee4fcda3ed7
David <bi-crosseur@hotmail.fr>
2021-04-04 18:17:51 +0200

    Add interactions resolved objects (#54)

---
+++ src/main/java/discord4j/discordjson/json/ApplicationCommandInteractionResolvedData.java
@@ -0,0 +1,27 @@
+package discord4j.discordjson.json;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import discord4j.discordjson.possible.Possible;
+import org.immutables.value.Value;
+
+import java.util.Map;
+
+@Value.Immutable
+@JsonSerialize(as = ImmutableApplicationCommandInteractionResolvedData.class)
+@JsonDeserialize(as = ImmutableApplicationCommandInteractionResolvedData.class)
+public interface ApplicationCommandInteractionResolvedData {
+
+    static ImmutableApplicationCommandInteractionResolvedData.Builder builder() {
+        return ImmutableApplicationCommandInteractionResolvedData.builder();
+    }
+
+    Possible<Map<String, UserData>> users();
+
+    Possible<Map<String, ResolvedMemberData>> members();
+
+    Possible<Map<String, ResolvedChannelData>> channels();
+
+    Possible<Map<String, RoleData>> roles();
+
+}
