ee08ba33e4b7aacd3cf102f5aa213d580a911ffa
David <bi-crosseur@hotmail.fr>
2021-04-06 12:18:30 +0200

    Update GuildWelcomeScreenModifyRequest.java

--- src/main/java/discord4j/discordjson/json/GuildWelcomeScreenModifyRequest.java
+++ src/main/java/discord4j/discordjson/json/GuildWelcomeScreenModifyRequest.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import discord4j.discordjson.possible.Possible;
 import org.immutables.value.Value;
 
+import java.util.List;
 import java.util.Optional;
 
 @Value.Immutable
@@ -20,7 +21,7 @@ public interface GuildWelcomeScreenModifyRequest {
     Possible<Optional<Boolean>> enabled();
 
     @JsonProperty("welcome_channels")
-    Possible<Optional<WelcomeScreenChannelData>> welcomeChannels();
+    Possible<Optional<List<WelcomeScreenChannelData>>> welcomeChannels();
 
     Possible<Optional<String>> description();
 
