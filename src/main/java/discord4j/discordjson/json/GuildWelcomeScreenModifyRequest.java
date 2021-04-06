7dc586b56fc6efca71648488d3f9a63e54ba388e
David <bi-crosseur@hotmail.fr>
2021-04-06 11:22:32 +0200

    Adds WelcomeScreen

---
+++ src/main/java/discord4j/discordjson/json/GuildWelcomeScreenModifyRequest.java
@@ -0,0 +1,27 @@
+package discord4j.discordjson.json;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import discord4j.discordjson.possible.Possible;
+import org.immutables.value.Value;
+
+import java.util.Optional;
+
+@Value.Immutable
+@JsonSerialize(as = ImmutableGuildWelcomeScreenModifyRequest.class)
+@JsonDeserialize(as = ImmutableGuildWelcomeScreenModifyRequest.class)
+public interface GuildWelcomeScreenModifyRequest {
+
+    static ImmutableGuildWelcomeScreenModifyRequest.Builder builder() {
+        return ImmutableGuildWelcomeScreenModifyRequest.builder();
+    }
+
+    Possible<Optional<Boolean>> enabled();
+
+    @JsonProperty("welcome_channels")
+    Possible<Optional<WelcomeScreenChannelData>> welcomeChannels();
+
+    Possible<Optional<String>> description();
+
+}
