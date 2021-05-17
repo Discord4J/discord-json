acfbda33252271133f63073cd6dbd744a6b0455f
David <bi-crosseur@hotmail.fr>
2021-05-17 11:06:07 +0200

    Add InviteData#expires_at
    https://discord.com/developers/docs/resources/invite#invite-object-invite-structure

--- src/main/java/discord4j/discordjson/json/InviteData.java
+++ src/main/java/discord4j/discordjson/json/InviteData.java
@@ -6,6 +6,8 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import discord4j.discordjson.possible.Possible;
 import org.immutables.value.Value;
 
+import java.util.Optional;
+
 @Value.Immutable
 @JsonSerialize(as = ImmutableInviteData.class)
 @JsonDeserialize(as = ImmutableInviteData.class)
@@ -39,6 +41,9 @@ public interface InviteData {
 
     @JsonProperty("approximate_member_count")
     Possible<Integer> approximateMemberCount();
+
+    @JsonProperty("expires_at")
+    Possible<Optional<String>> expiresAt();
 
     Possible<Integer> uses();
 
