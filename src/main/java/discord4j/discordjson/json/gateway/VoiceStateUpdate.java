f0be942e097078f07c6d3e62d44cc96f2cd91340
Axel Joly <azn9.pro@gmail.com>
2021-10-04 17:07:31 +0200

    Add missing fields on UpdateUserVoiceStateRequest and VoiceStateUpdate

--- src/main/java/discord4j/discordjson/json/gateway/VoiceStateUpdate.java
+++ src/main/java/discord4j/discordjson/json/gateway/VoiceStateUpdate.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import discord4j.discordjson.Id;
+import discord4j.discordjson.json.MemberData;
+import discord4j.discordjson.possible.Possible;
 import org.immutables.value.Value;
 
 import java.util.Optional;
@@ -17,12 +19,25 @@ public interface VoiceStateUpdate extends PayloadData {
         return ImmutableVoiceStateUpdate.builder();
     }
 
+    @JsonProperty("user_id")
+    Id userId();
+
+    Possible<MemberData> member();
+
     @JsonProperty("guild_id")
     Id guildId();
+
     @JsonProperty("channel_id")
     Optional<Id> channelId();
+
     @JsonProperty("self_mute")
     boolean selfMute();
+
     @JsonProperty("self_deaf")
     boolean selfDeaf();
+
+    boolean suppress();
+
+    @JsonProperty("request_to_speak_timestamp")
+    Optional<String> requestToSpeakTimestamp();
 }
