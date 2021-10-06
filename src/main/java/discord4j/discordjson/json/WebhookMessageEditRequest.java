a0471e98e8c16d31e6fbf543bcbf53610eebef11
David Richey <darichey1@gmail.com>
2021-10-06 01:43:46 -0500

    WebhookMessageEditRequest extend MessageRequestBase

--- src/main/java/discord4j/discordjson/json/WebhookMessageEditRequest.java
+++ src/main/java/discord4j/discordjson/json/WebhookMessageEditRequest.java
@@ -1,6 +1,5 @@
 package discord4j.discordjson.json;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import discord4j.discordjson.possible.Possible;
@@ -12,20 +11,11 @@ import java.util.Optional;
 @Value.Immutable
 @JsonSerialize(as = ImmutableWebhookMessageEditRequest.class)
 @JsonDeserialize(as = ImmutableWebhookMessageEditRequest.class)
-public interface WebhookMessageEditRequest {
+public interface WebhookMessageEditRequest extends MessageRequestBase {
 
     static ImmutableWebhookMessageEditRequest.Builder builder() {
         return ImmutableWebhookMessageEditRequest.builder();
     }
 
-    Possible<Optional<String>> content();
-
-    Possible<Optional<List<EmbedData>>> embeds();
-
-    @JsonProperty("allowed_mentions")
-    Possible<Optional<AllowedMentionsData>> allowedMentions();
-
     Possible<Optional<List<AttachmentData>>> attachments();
-
-    Possible<List<ComponentData>> components();
 }
