d96e72704d812578600176ef6656e6629bee3d73
SimonIT <simonit.orig@gmail.com>
2022-03-28 23:43:37 +0200

    Add missing params for threads without message https://discord.com/developers/docs/resources/channel#start-thread-without-message

--- src/main/java/discord4j/discordjson/json/StartThreadRequest.java
+++ src/main/java/discord4j/discordjson/json/StartThreadRequest.java
@@ -18,4 +18,21 @@ public interface StartThreadRequest {
 
     @JsonProperty("auto_archive_duration")
     int autoArchiveDuration();
+
+    /**
+     * the type of thread to create
+     * Only available without message
+     */
+    int type();
+
+    /**
+     * whether non-moderators can add other non-moderators to a thread; only available when creating a private thread
+     */
+    boolean invitable();
+
+    /**
+     * amount of seconds a user has to wait before sending another message (0-21600)
+     */
+    @JsonProperty("rate_limit_per_user")
+    int rateLimitPerUser();
 }
