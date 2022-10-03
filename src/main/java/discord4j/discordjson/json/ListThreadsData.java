c457355282ded2856241aa68ca5e7edf5647ae3a
skat <fishmoders@petruchio.org.ru>
2022-10-03 22:30:01 +0300

    Wrap ListThreadsData#hasMore() to Possible

--- src/main/java/discord4j/discordjson/json/ListThreadsData.java
+++ src/main/java/discord4j/discordjson/json/ListThreadsData.java
@@ -3,6 +3,7 @@ package discord4j.discordjson.json;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import discord4j.discordjson.possible.Possible;
 import org.immutables.value.Value;
 
 import java.util.List;
@@ -21,5 +22,5 @@ public interface ListThreadsData {
     List<ThreadMemberData> members();
 
     @JsonProperty("has_more")
-    boolean hasMore();
+    Possible<Boolean> hasMore();
 }
