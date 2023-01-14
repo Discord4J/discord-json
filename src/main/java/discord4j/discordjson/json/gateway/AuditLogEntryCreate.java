b1234305d2e4ab4665f3feb1c3b415c3ad8e414f
Doc <nachito94@msn.com>
2023-01-14 20:16:58 -0300

    Remove unused import

--- src/main/java/discord4j/discordjson/json/gateway/AuditLogEntryCreate.java
+++ src/main/java/discord4j/discordjson/json/gateway/AuditLogEntryCreate.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import discord4j.discordjson.Id;
 import discord4j.discordjson.json.AuditLogEntryData;
-import discord4j.discordjson.possible.Possible;
 import org.immutables.value.Value;
 
 @Value.Immutable
