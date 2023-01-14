2a54cdafebe149772eea731f6c4778714978c0d4
Doc <nachito94@msn.com>
2023-01-14 17:40:02 -0300

    Implement AuditLog Entry Create data

---
+++ src/main/java/discord4j/discordjson/json/gateway/AuditLogEntryCreate.java
@@ -0,0 +1,23 @@
+package discord4j.discordjson.json.gateway;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import discord4j.discordjson.Id;
+import discord4j.discordjson.json.AuditLogEntryData;
+import discord4j.discordjson.possible.Possible;
+import org.immutables.value.Value;
+
+@Value.Immutable
+@JsonSerialize(as = ImmutableAuditLogEntryCreate.class)
+@JsonDeserialize(as = ImmutableAuditLogEntryCreate.class)
+public interface AuditLogEntryCreate extends Dispatch, AuditLogEntryData {
+
+    static ImmutableAuditLogEntryCreate.Builder builder() {
+        return ImmutableAuditLogEntryCreate.builder();
+    }
+
+    @JsonProperty("guild_id")
+    Possible<Id> guildId();
+
+}
