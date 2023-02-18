91018bcd6781b665a82e15d3f6ad61210f87334e
dominoxp <dominoxplp@gmail.com>
2023-02-18 19:01:08 +0100

    used already present GuildApplicationCommandPermissionsData object, instead of creating a new one

--- src/main/java/discord4j/discordjson/json/gateway/ApplicationCommandPermissionUpdate.java
+++ src/main/java/discord4j/discordjson/json/gateway/ApplicationCommandPermissionUpdate.java
@@ -3,7 +3,7 @@ package discord4j.discordjson.json.gateway;
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import discord4j.discordjson.json.ApplicationCommandGuildPermissionsData;
+import discord4j.discordjson.json.GuildApplicationCommandPermissionsData;
 import org.immutables.value.Value;
 
 @Value.Immutable
@@ -16,5 +16,5 @@ public interface ApplicationCommandPermissionUpdate extends Dispatch {
     }
 
     @JsonUnwrapped
-    ApplicationCommandGuildPermissionsData data();
+    GuildApplicationCommandPermissionsData data();
 }
