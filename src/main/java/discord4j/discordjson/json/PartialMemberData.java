d6fce95983e5dbd24511bc5deb9af48830b274ce
Pedro <3602279+Doc94@users.noreply.github.com>
2023-02-26 23:33:52 -0300

    Add flags field to Guild Member data (#135)

--- src/main/java/discord4j/discordjson/json/PartialMemberData.java
+++ src/main/java/discord4j/discordjson/json/PartialMemberData.java
@@ -41,4 +41,8 @@ public interface PartialMemberData {
 
     @JsonProperty("communication_disabled_until")
     Possible<Optional<String>> communicationDisabledUntil();
+
+    default long flags() {
+        return 0L;
+    }
 }
