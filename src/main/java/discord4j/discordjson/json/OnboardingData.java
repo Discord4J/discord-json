ff8b7199ea354db7eef22280bd6f9bcf98e6a75f
Doc <nachito94@msn.com>
2023-04-07 23:03:59 -0400

    Implement structure for Onboarding data

---
+++ src/main/java/discord4j/discordjson/json/OnboardingData.java
@@ -0,0 +1,29 @@
+package discord4j.discordjson.json;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import discord4j.discordjson.Id;
+import java.util.List;
+import org.immutables.value.Value;
+
+@Value.Immutable
+@JsonSerialize(as = ImmutableOnboardingData.class)
+@JsonDeserialize(as = ImmutableOnboardingData.class)
+public interface OnboardingData {
+
+    static ImmutableOnboardingData.Builder builder() {
+        return ImmutableOnboardingData.builder();
+    }
+
+    @JsonProperty("guild_id")
+    Id guildId();
+
+    List<OnboardingPromptData> prompts();
+
+    @JsonProperty("default_channel_ids")
+    List<Id> defaultChannelsIds();
+
+    boolean enabled();
+
+}
