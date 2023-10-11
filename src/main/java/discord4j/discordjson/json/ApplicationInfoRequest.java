6effa27fdb596e2408cab3224da089548a745455
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-10-11 14:08:27 -0700

    Edit Current Application
    Adds support for https://discord.com/developers/docs/resources/application#edit-current-application

---
+++ src/main/java/discord4j/discordjson/json/ApplicationInfoRequest.java
@@ -0,0 +1,44 @@
+package discord4j.discordjson.json;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import discord4j.discordjson.possible.Possible;
+import org.immutables.value.Value;
+
+import java.util.List;
+
+@Value.Immutable
+@JsonSerialize(as = ImmutableApplicationInfoRequest.class)
+@JsonDeserialize(as = ImmutableApplicationInfoRequest.class)
+public interface ApplicationInfoRequest {
+
+    static ImmutableApplicationInfoRequest.Builder builder() {
+        return ImmutableApplicationInfoRequest.builder();
+    }
+
+    @JsonProperty("custom_install_url")
+    Possible<String> customInstallUrl();
+
+    @JsonProperty("description")
+    Possible<String> description();
+
+    @JsonProperty("role_connections_verification_url")
+    Possible<String> roleConnectionsVerificationUrl();
+
+    @JsonProperty("install_params")
+    Possible<InstallParamsData> installParams();
+
+    Possible<Integer> flags();
+
+    Possible<byte[]> icon();
+
+    @JsonProperty("cover_image")
+    Possible<byte[]> coverImage();
+
+    @JsonProperty("interactions_endpoint_url")
+    Possible<String> interactionsEndpointUrl();
+
+    Possible<List<String>> tags();
+
+}
