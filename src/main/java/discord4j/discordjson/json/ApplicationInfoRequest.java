97853f61c577691d0d883400f8076626b444b93b
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-10-16 13:41:00 -0700

    use Possible<Optional<String>> for image data params

--- src/main/java/discord4j/discordjson/json/ApplicationInfoRequest.java
+++ src/main/java/discord4j/discordjson/json/ApplicationInfoRequest.java
@@ -7,6 +7,7 @@ import discord4j.discordjson.possible.Possible;
 import org.immutables.value.Value;
 
 import java.util.List;
+import java.util.Optional;
 
 @Value.Immutable
 @JsonSerialize(as = ImmutableApplicationInfoRequest.class)
@@ -31,10 +32,10 @@ public interface ApplicationInfoRequest {
 
     Possible<Integer> flags();
 
-    Possible<byte[]> icon();
+    Possible<Optional<String>> icon();
 
     @JsonProperty("cover_image")
-    Possible<byte[]> coverImage();
+    Possible<Optional<String>> coverImage();
 
     @JsonProperty("interactions_endpoint_url")
     Possible<String> interactionsEndpointUrl();
