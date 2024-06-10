9e8a1a30cbcead2ed35da783ecc09380d62dc886
Axel Joly <azn9.pro@gmail.com>
2024-06-10 16:40:15 -0400

    feat: add PollVote events data

---
+++ src/main/java/discord4j/discordjson/json/gateway/PollVoteRemove.java
@@ -0,0 +1,32 @@
+/*
+ * This file is part of Discord4J.
+ *
+ * Discord4J is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Discord4J is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Discord4J.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package discord4j.discordjson.json.gateway;
+
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import org.immutables.value.Value;
+
+@Value.Immutable(builder = false)
+@JsonSerialize(as = ImmutablePollVoteRemove.class)
+@JsonDeserialize(as = ImmutablePollVoteRemove.class)
+public interface PollVoteRemove extends Dispatch {
+
+    @JsonUnwrapped
+    PollVoteData data();
+
+}
