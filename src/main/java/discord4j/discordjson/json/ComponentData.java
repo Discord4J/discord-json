a4c511ad022b9598b4811682cbcae1239906fdf7
Axel Joly <azn9.pro@gmail.com>
2025-03-01 19:17:40 +0100

    fix: add missing field 'id' for components (#199)

--- src/main/java/discord4j/discordjson/json/ComponentData.java
+++ src/main/java/discord4j/discordjson/json/ComponentData.java
@@ -19,6 +19,8 @@ public interface ComponentData {
         return ImmutableComponentData.builder();
     }
 
+    Possible<Integer> id();
+
     int type();
 
     Possible<List<ComponentData>> components();
