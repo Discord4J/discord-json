418d331e91ade68a559c20da3641399492a4ea70
Doc <nachito94@msn.com>
2026-02-14 11:40:29 -0300

    feat: add community invite fields

--- src/main/java/discord4j/discordjson/json/InviteCreateRequest.java
+++ src/main/java/discord4j/discordjson/json/InviteCreateRequest.java
@@ -3,8 +3,11 @@ package discord4j.discordjson.json;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import discord4j.discordjson.Id;
 import discord4j.discordjson.possible.Possible;
 import org.immutables.value.Value;
+
+import java.util.List;
 
 @Value.Immutable
 @JsonSerialize(as = ImmutableInviteCreateRequest.class)
@@ -33,5 +36,14 @@ public interface InviteCreateRequest {
 
     @JsonProperty("target_application_id")
     Possible<String> targetApplicationId();
+
+    @JsonProperty("target_users_file")
+    Possible<String> targetUsersFile();
+
+    @JsonProperty("payload_json")
+    Possible<String> payloadJson();
+
+    @JsonProperty("role_ids")
+    Possible<List<Id>> roleIds();
 
 }
