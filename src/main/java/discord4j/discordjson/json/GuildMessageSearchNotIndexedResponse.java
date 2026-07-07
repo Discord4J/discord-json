4d8553ff40a7c1064ac340cf23860e29fc10f486
Axel Joly <azn9.pro@gmail.com>
2026-07-07 03:45:20 +0200

    feat: guild message search data (#223)
    Co-authored-by: Doc <git@mrdoc.dev>

---
+++ src/main/java/discord4j/discordjson/json/GuildMessageSearchNotIndexedResponse.java
@@ -0,0 +1,23 @@
+package discord4j.discordjson.json;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import org.immutables.value.Value;
+
+@Value.Immutable
+@JsonSerialize(as = ImmutableGuildMessageSearchNotIndexedResponse.class)
+@JsonDeserialize(as = ImmutableGuildMessageSearchNotIndexedResponse.class)
+public interface GuildMessageSearchNotIndexedResponse {
+
+    String message();
+
+    int code();
+
+    @JsonProperty("documents_indexed")
+    int documentsIndexed();
+
+    @JsonProperty("retry_after")
+    long retryAfter();
+
+}
